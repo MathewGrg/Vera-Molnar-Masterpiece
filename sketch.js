661cc15582231a410c212f8041c315e6edb673d7
Mathew George <mathewgrg@gmail.com>
2020-01-28 18:53:35 -0500

    Added a nested for loop

--- sketch.js
+++ sketch.js
@@ -15,215 +15,41 @@ function draw()
     
     // first row of rectangles
     strokeWeight(2);
-    for (var i=0; i<9; i++)
+    
+    
+    for (var j=0; j<7; j++)
         {
-            if(i>6)
+            for (var i=0; i<9; i++)
                 {
-                    stroke(220, 200, 255);
-                    r = 1;
+                    if(i>6)
+                        {
+                            stroke(10*i*j, 200, 255);
+                            r = 1+j;
+                        }
+                    else if(i>4)
+                        {
+                            stroke(255, 100, 0*i*j);
+                            r = 5+j;
+                        }
+                    else if(i>3)
+                        {
+                            stroke(155, 200, 10*i*j);
+                            r = 5+j;
+                        }
+                    else if(i>1)
+                        {
+                            stroke(155, 10*i*j, 100);
+                            r = j;
+                        }
+                    else
+                        {
+                            stroke(20*j*i, 10, 220);
+                            r = 1+j;
+                        }
+
+                    rect(80+ i*80 + random(-r, r), 
+                         80+ j*80 + random(-r, r), 
+                         60, 60);
                 }
-            else if(i>3)
-                {
-                    stroke(255, 100, 0);
-                    r = 5;
-                }
-            else if(i>1)
-                {
-                    stroke(155, 0, 100);
-                    r = 5;
-                }
-            else
-                {
-                    stroke(150);
-                    r = 1;
-                }
-            
-            rect(80+ i*80 + random(-r, r), 
-                 80 + random(-r, r), 
-                 60, 60);
-        }
-    
-    
-    // 2nd row of rectangles
-    strokeWeight(4);
-    for (var i=0; i<9; i++)
-        {
-            if(i>6)
-                {
-                    stroke(220, 200, 255);
-                    r = 2;
-                }
-            else if(i>4)
-                {
-                    stroke(255, 100, 0);
-                    r = 15;
-                }
-            else if(i>0)
-                {
-                    stroke(155, 0, 100);
-                    r = 5;
-                }
-            else
-                {
-                    stroke(150);
-                    r = 2;
-                }
-            
-            rect(80+ i*80 + random(-r, r), 
-                 160 + random(-r, r), 
-                 60, 60);
-        }
-    
-    // 3rd row of rectangles
-    strokeWeight(6);
-    for (var i=0; i<9; i++)
-        {
-            if(i>6)
-                {
-                    stroke(220, 200, 255);
-                    r = 3;
-                }
-            else if(i>4)
-                {
-                    stroke(255, 100, 0);
-                    r = 10;
-                }
-            else if(i>0)
-                {
-                    stroke(155, 0, 100);
-                    r = 10;
-                }
-            else
-                {
-                    stroke(150);
-                    r = 3;
-                }
-            
-            rect(80+ i*80 + random(-r, r), 
-                 240 + random(-r, r), 
-                 60, 60);
-        }
-    
-    
-    // 4th row of rectangles
-    strokeWeight(8);
-    for (var i=0; i<9; i++)
-        {
-            if(i>6)
-                {
-                    stroke(220, 200, 255);
-                    r = 5;
-                }
-            else if(i>4)
-                {
-                    stroke(255, 100, 0);
-                    r = 15;
-                }
-            else if(i>0)
-                {
-                    stroke(155, 0, 100);
-                    r = 15;
-                }
-            else
-                {
-                    stroke(150);
-                    r = 5;
-                }
-            
-            rect(80+ i*80 + random(-r, r), 
-                 320 + random(-r, r), 
-                 60, 60);
-        }
-    
-    // 5th row of rectangles
-    strokeWeight(6);
-    for (var i=0; i<9; i++)
-        {
-            if(i>6)
-                {
-                    stroke(220, 200, 255);
-                    r = 3;
-                }
-            else if(i>4)
-                {
-                    stroke(255, 100, 0);
-                    r = 10;
-                }
-            else if(i>0)
-                {
-                    stroke(155, 0, 100);
-                    r = 10;
-                }
-            else
-                {
-                    stroke(150);
-                    r = 3;
-                }
-            
-            rect(80+ i*80 + random(-r, r), 
-                 400 + random(-r, r), 
-                 60, 60);
-        }
-    
-    
-    // 6th row of rectangles
-    strokeWeight(4);
-    for (var i=0; i<9; i++)
-        {
-            if(i>6)
-                {
-                    stroke(220, 200, 255);
-                    r = 2;
-                }
-            else if(i>4)
-                {
-                    stroke(255, 100, 0);
-                    r = 7;
-                }
-            else if(i>0)
-                {
-                    stroke(155, 0, 100);
-                    r = 7;
-                }
-            else
-                {
-                    stroke(150);
-                    r = 2;
-                }
-            
-            rect(80+ i*80 + random(-r, r), 
-                 480 + random(-r, r), 
-                 60, 60);
-        }
-    
-    
-    // 7th row of rectangles
-    strokeWeight(2);
-    for (var i=0; i<9; i++)
-        {
-            if(i>6)
-                {
-                    stroke(220, 200, 255);
-                    r = 1;
-                }
-            else if(i==4 || i==5)
-                {
-                    stroke(255, 100, 0);
-                    r = 5;
-                }
-            else if(i>0)
-                {
-                    stroke(155, 0, 100);
-                    r = 5;
-                }
-            else
-                {
-                    stroke(150);
-                    r = 1;
-                }
-            
-            rect(80+ i*80 + random(-r, r), 
-                 560 + random(-r, r), 
-                 60, 60);
         }
 }
